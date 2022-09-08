1de72f91ba1afb9ed139d0788f797cf274029423
Grant-Ellington <gellingtonem6@gmail.com>
2022-09-08 17:05:36 -0500

    started js file; can fetch location and weather from openWeatherAPI

--- assets/script.js/script.js
+++ assets/script.js/script.js
@@ -1,4 +1,23 @@
-fetch('https://api.openweathermap.org/data/2.5/onecall?lat=36.162663&lon=-86.781601&exclude=hourly,daily&appid=74ae31b27694c400a00f630e449b8981', {
+var today = document.getElementById('today');
+var fiveDay = document.getElementById('fiveday');
+var searchBtn = document.getElementById('searchBtn');
+var searchInput = document.getElementById('searchInput')
+
+var forSubmitHandler =  function(event) {
+    event.preventDefault()
+
+    location = searchinput.value.trim()
+
+    if(location) {
+        findCoordinates(location)
+
+        today.textContent = '';
+        searchInput.value = '';
+    }
+
+}
+
+fetch('http://api.openweathermap.org/geo/1.0/direct?q=Tennessee&limit=5&appid=8e63ee673b69ef9b660b0e01ef8bfc2a', {
   // The browser fetches the resource from the remote server without first looking in the cache.
   // The browser will then update the cache with the downloaded resource.
   cache: 'reload',
