9e63672158745108c7faa25524ea8e4ea05e124a
Grant-Ellington <gellingtonem6@gmail.com>
2022-09-12 10:23:05 -0500

    display of todays weather done; need to complete five day forcast annd button

--- assets/script.js/script.js
+++ assets/script.js/script.js
@@ -4,11 +4,14 @@ var searchBtn = document.getElementById('searchBtn');
 var searchInput = document.getElementById('searchInput')
 var searchBtn = document.getElementById('searchBtn')
 
+let cityName = document.getElementById('cityName')
+let tempToday = document.getElementById('tempToday')
+let weatherToday = document.getElementById('weatherToday')
 var key = '74ae31b27694c400a00f630e449b8981';
 
-let city = ''
+let city = 'Nashville'
 
-searchBtn.addEventListener('click', submitCity);
+/*searchBtn.addEventListener('click', getLocation)
 
 function submitCity () {
   let city = searchInput.value
@@ -24,7 +27,7 @@ submitCity();
 
 function logFunction () {
   console.log('it works');
-}
+}*/
 
 
 function getLocation() {
@@ -33,11 +36,17 @@ function getLocation() {
   fetch(cordinateAPI)
     .then(response => response.json())
     .then(resultsLoc => {
+
+      console.log(resultsLoc)
       let lat = resultsLoc[0].lat
-      console.log(results[0].lat)
+      console.log(resultsLoc[0].lat)
       let lon = resultsLoc[0].lon
       console.log(lon)
       getWeather(lat, lon)
+
+      let name = resultsLoc[0].name
+      console.log(name)
+      cityName.innerHTML = name      
     }
 
     )
@@ -52,12 +61,22 @@ function getWeather (lat, lon) {
   fetch(weatherAPI)
     .then(response =>response.json())
     .then(resultsWeather => {
-      console.log(resultsWeather.main
-        )
+      console.log(resultsWeather)
+
+      let temp = (resultsWeather.main.temp - 273.15) * 1.8 + 32
+      console.log(temp)
+      tempToday.innerHTML = 'Temperature:'+temp +' ˚ F'
+
+      let weather = resultsWeather.weather[0].description
+      console.log(weather)
+      weatherToday.innerHTML = 'Weather: ' + weather
     })
 }
-getLocation()
+getLocation(city);
 
+function createTodayData (){
+
+}
 /*fetch('http://api.openweathermap.org/geo/1.0/direct?q=Tennessee&limit=5&appid=8e63ee673b69ef9b660b0e01ef8bfc2a', {
   // The browser fetches the resource from the remote server without first looking in the cache.
   // The browser will then update the cache with the downloaded resource.
