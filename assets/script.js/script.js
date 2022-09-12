49bf9bbc2637b036814f0199c382628ffd260b3e
Grant-Ellington <gellingtonem6@gmail.com>
2022-09-12 11:35:35 -0500

    search bar and btn connected to API and will load today data, need to complete five day, having problem with API request

--- assets/script.js/script.js
+++ assets/script.js/script.js
@@ -7,30 +7,69 @@ var searchBtn = document.getElementById('searchBtn')
 let cityName = document.getElementById('cityName')
 let tempToday = document.getElementById('tempToday')
 let weatherToday = document.getElementById('weatherToday')
+
+const nashvilleBtn = document.querySelector('#nashvilleBtn')
+const newYorkBtn = document.querySelector('#newYorkBtn')
+const lasVegasBtn = document.querySelector('#lasVegasBtn')
+const chicagoBtn = document.querySelector('#chicagoBtn')
+const laBtn = document.querySelector('#laBtn')
+
 var key = '74ae31b27694c400a00f630e449b8981';
 
-let city = 'Nashville'
+//Nashville Button
+nashvilleBtn.addEventListener('click', function () {
+  console.log('Nashville')
+  let city = ('Nashville')
+  getLocation(city)
+})
 
-/*searchBtn.addEventListener('click', getLocation)
+// New York button
+newYorkBtn.addEventListener('click', function () {
+  console.log('Nashville')
+  let city = ('New York')
+  getLocation(city)
+})
 
-function submitCity () {
-  let city = searchInput.value
-  if(city) {
-    getLocation(city)
-  } else {
-    alert('This is not a avaliable city')
-  }
-}
+// Las Vegas btn
+lasVegasBtn.addEventListener('click', function () {
+  console.log('Nashville')
+  let city = ('Las Vegas')
+  getLocation(city)
+})
 
-submitCity();
+//Chicago Btn
+chicagoBtn.addEventListener('click', function () {
+  console.log('Nashville')
+  let city = ('Chicago')
+  getLocation(city)
+})
+
+// LA btn
+laBtn.addEventListener('click', function () {
+  console.log('Nashville')
+  let city = ('Las Angeles')
+  getLocation(city)
+})
+
+
+
+searchBtn.addEventListener('click', function () {
+
+  console.log('it works')
+  let city = searchInput.value.trim()
+  getLocation(city);
+
+})
+
+
 
 
 function logFunction () {
   console.log('it works');
-}*/
+}
 
 
-function getLocation() {
+function getLocation(city) {
   let cordinateAPI = "https://api.openweathermap.org/geo/1.0/direct?q="+ city +"&limit=5&appid=" +key;
 
   fetch(cordinateAPI)
@@ -43,6 +82,7 @@ function getLocation() {
       let lon = resultsLoc[0].lon
       console.log(lon)
       getWeather(lat, lon)
+      //getFiveDay(lat, lon)
 
       let name = resultsLoc[0].name
       console.log(name)
@@ -52,8 +92,6 @@ function getLocation() {
     )
   
 }
-
-getLocation();
 
 function getWeather (lat, lon) {
   let weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=' + key;
@@ -72,11 +110,17 @@ function getWeather (lat, lon) {
       weatherToday.innerHTML = 'Weather: ' + weather
     })
 }
-getLocation(city);
 
-function createTodayData (){
+/*function getFiveDay (lat, lon) {
+   let fiveDayAPI = 'api.openweathermap.org/data/2.5/forecast/daily?lat='+lat+'&lon='+lon+'&cnt={cnt}&appid=74ae31b27694c400a00f630e449b8981';
 
-}
+   fetch(fiveDayAPI)
+   .then(response => response.json())
+   .then(resultsFiveDay => {
+    console.log(resultsFiveDay)
+   })
+
+}*/
 /*fetch('http://api.openweathermap.org/geo/1.0/direct?q=Tennessee&limit=5&appid=8e63ee673b69ef9b660b0e01ef8bfc2a', {
   // The browser fetches the resource from the remote server without first looking in the cache.
   // The browser will then update the cache with the downloaded resource.
